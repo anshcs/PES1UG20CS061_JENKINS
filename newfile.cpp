7c61db65b22f7d2b3eb2bb6156fe59f8de85b766
Ansh cs <76522728+anshcs@users.noreply.github.com>
2023-02-17 10:29:40 +0530

    Create newfile.cpp

---
+++ newfile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "My SRN is PES1UG20CS061\n";
+  cout <<"This is for task 5.\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
